98c655663ed43e6733ea23eee1188e736fde8a0c
RahulB <rahulbisht1012@gmail.com>
2024-02-19 00:12:06 +0530

    Sidebar Completed [Description]
    Completed the sidebar as per the design.Currently it is desktop
    friendly and not mobile responsive but once the desktop part is
    done. we will work on mobibe responsiveness as well.
    
    RahulB

--- app/_components/Content.js
+++ app/_components/Content.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Content = () => {
-    return <div className="w-full">Content</div>;
+    return (
+        <>
+            <div className="w-full bg-white rounded-2xl">Projects</div>
+        </>
+    );
 };
 
 export default Content;
