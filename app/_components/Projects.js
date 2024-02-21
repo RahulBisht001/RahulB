07c198060835062cf340aa27fd43ec6c9942791b
RahulB <rahulbisht1012@gmail.com>
2024-02-21 15:05:07 +0530

    Working on remaining parts.
    Built contact section.working on blog and project section.

--- app/_components/Projects.js
+++ app/_components/Projects.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Projects = () => {
-    return <div>Projects</div>;
+    return (
+        <>
+            <div className="p-2">
+                <div className="flex items-center gap-5 sm:mx-5">
+                    <h1 className="text-xl font-bold">PROJECTS</h1>
+
+                    <div className="w-[20%] h-1 bg-gray-200 rounded-xl">
+                        <div className="h-1 rounded-xl bg-gradient-to-r from-[#FF9C1A] to-[#f92c1a]"></div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Projects;
