870b4432934e47caeaf9274e49986ac470db0620
RahulB <rahulbisht1012@gmail.com>
2024-02-23 01:38:36 +0530

    Project Section Done

---
+++ app/_utils/Domains.js
@@ -0,0 +1,24 @@
+const domains = [
+    {
+        id: 0,
+        title: "All",
+    },
+    {
+        id: 1,
+        title: "Web Development",
+    },
+    {
+        id: 2,
+        title: "Machine Learning",
+    },
+    {
+        id: 3,
+        title: "API Development",
+    },
+    {
+        id: 4,
+        title: "React Native",
+    },
+];
+
+export default domains;
